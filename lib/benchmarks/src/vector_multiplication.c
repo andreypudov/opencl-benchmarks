cc667e5c3c06c429b3979ea940ba1e272521f3f0
Andrey Pudov <mail@andreypudov.com>
2021-04-16 15:50:22 +0300

    ~ uses release mode for the build

--- lib/benchmarks/src/vector_multiplication.c
+++ lib/benchmarks/src/vector_multiplication.c
@@ -1,7 +1,7 @@
 #include "vector_multiplication.h"
 
 void multiply(int *vector_a, int *vector_b, int *vector_c, size_t length) {
-   for (int index = 0; index < length; ++index) {
-      vector_c[index] = vector_a[index] * vector_b[index];
-   }
+  for (int index = 0; index < length; ++index) {
+    vector_c[index] = vector_a[index] * vector_b[index];
+  }
 }
--- src/opencl-benchmarks/src/vector_multiplication.cc
+++ src/opencl-benchmarks/src/vector_multiplication.cc
@@ -1,16 +1,8 @@
 #include <benchmark/benchmark.h>
 
-static void BM_StringCreation(benchmark::State& state) {
+static void CPU_VectorMultiplication(benchmark::State& state) {
   for (auto _ : state)
     std::string empty_string;
 }
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
 
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  for (auto _ : state)
-    std::string copy(x);
-}
-BENCHMARK(BM_StringCopy);
+BENCHMARK(CPU_VectorMultiplication);

7d2762871b72ead8ebe8d104db1ef86cce6c5186
Andrey Pudov <mail@andreypudov.com>
2021-04-23 23:42:06 +0300

    ~ improves compilation procesure

--- test/lib/benchmarks/src/benchmarks.cc
+++ test/lib/benchmarks/src/benchmarks.cc
@@ -1,9 +1,5 @@
 #include "gtest/gtest.h"
 
-TEST(vector_multiplication, multiply) {
-  EXPECT_EQ(1, 1);
-}
-
 auto main(int argc, char *argv[]) -> decltype(RUN_ALL_TESTS()) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
--- test/lib/benchmarks/src/vector_multiplication.cc
+++ test/lib/benchmarks/src/vector_multiplication.cc
@@ -1,5 +1,19 @@
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+extern "C" {
+#include "vector_multiplication.h"
+}
 
 TEST(vector_multiplication, multiply) {
-  EXPECT_EQ(1, 1);
+  const size_t length = 8;
+
+  int vector_a[length] = {0, 1, 2, 3, 4, 5, 6, 7};
+  int vector_b[length] = {7, 6, 5, 4, 3, 2, 1, 0};
+  int vector_c[length];
+  int expected_c[length] = {0, 6, 10, 12, 12, 10, 6, 0};
+
+  multiply(vector_a, vector_b, vector_c, length);
+
+  EXPECT_THAT(expected_c, testing::ContainerEq(vector_c));
 }

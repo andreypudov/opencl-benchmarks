6a3b1f304e43dd2006b9435fd192d72e5ee7c5f2
Andrey Pudov <mail@andreypudov.com>
2021-04-24 19:09:50 +0300

    ~ adds benchmark for vector multiplication

--- src/opencl-benchmarks/src/vector_multiplication.cc
+++ src/opencl-benchmarks/src/vector_multiplication.cc
@@ -1,8 +1,19 @@
 #include <benchmark/benchmark.h>
 
+extern "C" {
+#include "vector_multiplication.h"
+}
+
 static void CPU_VectorMultiplication(benchmark::State& state) {
-  for (auto _ : state)
-    std::string empty_string;
+  const size_t length = 8;
+
+  int vector_a[length] = {0, 1, 2, 3, 4, 5, 6, 7};
+  int vector_b[length] = {7, 6, 5, 4, 3, 2, 1, 0};
+  int vector_c[length];
+
+  for (auto _ : state) {
+    multiply(vector_a, vector_b, vector_c, length);
+  }
 }
 
 BENCHMARK(CPU_VectorMultiplication);
